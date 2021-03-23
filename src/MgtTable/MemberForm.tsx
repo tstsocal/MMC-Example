1ec130b8627e2b6cf88e14fbce0f1e68a5e500d6
rene-grigory <rene.tstsocal@gmail.com>
2021-03-22 20:44:38 -0700

    added useReducer

--- src/MgtTable/MemberForm.tsx
+++ src/MgtTable/MemberForm.tsx
@@ -1,37 +1,71 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import { IMember } from '../../types'
 import { Form, FormGroup, Label, Input } from 'reactstrap'
 
-export const MemberForm: React.FC<IMember> = ({
-  fname,
-  lname,
-  alias,
-  email,
-  role,
-  status,
-  reason
-}) => {
+interface IAction {
+  field: string
+  value: string
+}
+
+function reducer(state: IMember, action: IAction) {
+  return { ...state, [action.field]: action.value }
+}
+
+export const MemberForm: React.FC<IMember> = (props) => {
+  const [state, dispatch] = useReducer(reducer, props)
+
   return (
     <Form className="MemberForm">
       <FormGroup>
         <Label for="fname">First Name</Label>
-        <Input id="fname" defaultValue={fname} />
+        <Input
+          id="fname"
+          value={state.fname}
+          onChange={(e) =>
+            dispatch({ field: 'fname', value: e.currentTarget.value })
+          }
+        />
       </FormGroup>
       <FormGroup>
         <Label for="lname">Last Name</Label>
-        <Input id="lname" defaultValue={lname} />
+        <Input
+          id="lname"
+          value={state.lname}
+          onChange={(e) =>
+            dispatch({ field: 'lname', value: e.currentTarget.value })
+          }
+        />
       </FormGroup>
       <FormGroup>
         <Label for="alias">Alias</Label>
-        <Input id="alias" defaultValue={alias} />
+        <Input
+          id="alias"
+          value={state.alias}
+          onChange={(e) =>
+            dispatch({ field: 'alias', value: e.currentTarget.value })
+          }
+        />
       </FormGroup>
       <FormGroup>
         <Label for="email">Email</Label>
-        <Input id="email" defaultValue={email} />
+        <Input
+          id="email"
+          value={state.email}
+          onChange={(e) =>
+            dispatch({ field: 'email', value: e.currentTarget.value })
+          }
+        />
       </FormGroup>
       <FormGroup>
         <Label for="status">Status</Label>
-        <Input type="select" id="status" defaultValue={status}>
+        <Input
+          type="select"
+          id="status"
+          value={state.status}
+          onChange={(e) =>
+            dispatch({ field: 'status', value: e.currentTarget.value })
+          }
+        >
           <option>ACTIVE</option>
           <option>PENDING</option>
           <option>BANNED</option>
@@ -40,11 +74,26 @@ export const MemberForm: React.FC<IMember> = ({
       </FormGroup>
       <FormGroup>
         <Label for="role">Role</Label>
-        <Input id="role" disabled defaultValue={role} />
+        <Input
+          id="role"
+          disabled
+          value={state.role}
+          onChange={(e) =>
+            dispatch({ field: 'role', value: e.currentTarget.value })
+          }
+        />
       </FormGroup>
       <FormGroup>
         <Label for="reason">Rejected/Banned Reason</Label>
-        <Input id="reason" type="textarea" disabled defaultValue={reason} />
+        <Input
+          id="reason"
+          type="textarea"
+          disabled
+          value={state.reason}
+          onChange={(e) =>
+            dispatch({ field: 'reason', value: e.currentTarget.value })
+          }
+        />
       </FormGroup>
     </Form>
   )
